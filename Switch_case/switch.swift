e7bd1757103c3d8988778855a490d729eddd3010
harsh <93788928+harsh23079@users.noreply.github.com>
2022-12-23 01:09:37 +0530

    second

--- Switch_case/switch.swift
+++ Switch_case/switch.swift
@@ -4,24 +4,25 @@ var aNumber =  Int(readLine()!)!
 func dayOfTheWeek(day: Int) {
   
   //Write your code inside this function.
-  switch day{
-    case 1:
-    print("Monday")
-     case 2:
-    print("Tuesday")
-    case 3:
-    print("Wednesday")
-     case 4:
-    print("Thursday")
-    case 5:
-    print("Friday")
-     case 6:
-    print("Saturday")
-    default:
-    print("You Entered Wrong day number")
-    
-  }
-  
+    switch day{
+       case 1:
+       print("Monday")
+        case 2:
+       print("Tuesday")
+       case 3:
+       print("Wednesday")
+        case 4:
+       print("Thursday")
+       case 5:
+       print("Friday")
+        case 6:
+       print("Saturday")
+       case 7:
+       print("Sunday")
+       default:
+       print("Error")
+       
+     }
   
   
 }
