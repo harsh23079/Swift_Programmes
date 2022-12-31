82b3cbcaac0af315f5464a376a50c95936f35696
harsh <93788928+harsh23079@users.noreply.github.com>
2022-12-31 14:34:00 +0530

    return-function

---
+++ fucntion-3-return/returnFunction.playground/Contents.swift
@@ -0,0 +1,17 @@
+//Create your function here 👇
+
+func isOdd(n: Int) -> Bool {
+  if n%2 == 1 {
+    return true
+  }
+  else
+  {
+    return false
+  }
+}
+
+
+//Try some different numbers below:
+//The code below is not part of the exercise, but you can test your own code with different numbers.
+let numberIsOdd = isOdd(n: 53)
+print(numberIsOdd)
